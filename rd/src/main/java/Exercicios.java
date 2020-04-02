7fd49c6ec6b6a277b994546e401c2cb02bd3088c
kaaquino <63032465+kaaquino@users.noreply.github.com>
2020-04-02 00:56:50 -0300

    Add files via upload

---
+++ rd/src/main/java/Exercicios.java
@@ -0,0 +1,10 @@
+public class Exercicios {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 10;
+        while(a<b){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
